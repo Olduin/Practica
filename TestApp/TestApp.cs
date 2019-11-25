7446ac0e7e77f87f40e1c0d80031337a1b05a7a3
Вячеслав Коцюк <vkotsiuk@yandex.ru>
2019-11-25 19:59:26 +0400

    Создан класс приложения.

---
+++ TestApp/TestApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    /// <summary>
+    /// Основной класс приложения
+    /// </summary>
+    public class TestApp
+    {
+        /// <summary>
+        /// Метод запуска приложения
+        /// </summary>
+        public void Run()
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Метод создает экземпляр приложения и запускает его.
+        /// </summary>
+        public static void Main()
+        {
+            TestApp app = new TestApp();
+            app.Run();
+        }
+    }
+}
